cf10484d8b55cee80b456c86cb9bead28e3d4643
tristansarmiento <tris10sarmiento@gmail.com>
2026-07-11 21:21:26 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+!/bin/bash
+
+# Official repo packages
+pacman_pkgs=(
+        docker
+        docker-compose
+        spotify-launcher
+        brave-bin
+        pycharm-community-edition
+        obsidian
+        discord
+        steam
+        kitty
+        neovim
+        mullvad-vpn
+        git
+        yay
+
+)
+
+# AUR packages
+# aur_pkgs=(
+#
+# )
+
+echo "Installing pacman packages..."
+sudo pacman -S -- needed --noconfirm "${pacman_pkgs[@]}"
+
+# echo "Installing pacman packages..."
+# yay -S --needed --noconfirm "${aur_pkgs[@]}"
